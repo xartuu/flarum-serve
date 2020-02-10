85ca3bcae5613335feaf1ae2bda09ae4894dcf1d
Fajuu <fajuu@mykill.pl>
2020-02-10 01:37:06 +0100

    ⠀

---
+++ README.md
@@ -0,0 +1,63 @@
+<h1 align="center">Flarum Serve</h1>
+<p align="center">
+  <img src="https://img.shields.io/github/release/Fajuu/flarum-serve.svg" />
+  <img src="https://img.shields.io/github/release-date/Fajuu/flarum-serve.svg" />
+  <img src="https://img.shields.io/github/languages/top/Fajuu/flarum-serve.svg" />
+  <a href="https://packagist.org/packages/Fajuu/flarum-serve">
+    <img src="https://img.shields.io/packagist/dt/Fajuu/flarum-serve.svg" target="_blank" />
+  </a>
+  <a href="https://github.com/Fajuu/flarum-serve/blob/master/LICENSE">
+    <img src="https://img.shields.io/badge/license-MIT-yellow.svg" target="_blank" />
+  </a>
+</p>
+
+> Start Flarum in the PHP development server.
+
+## 📢 Important information
+
+- It is not an extension, so it does not display on the extension page.
+- Works immediately after installation (similar to `plug&play`).
+- You can set port and custom domain. For this purpose, use the arguments - `--host` and `-p, --port` (see screenshots).
+
+## 🚀 Installation
+
+To download the ~~extension~~ _tool_ you can ~~use [Bazaar](https://discuss.flarum.org/d/5151-flagrow-bazaar-the-extension-marketplace)~~ ~~or~~ _only_ install it through composer:
+
+```bash
+composer require fajuu/flarum-serve
+```
+
+## ✨ Demo
+
+Command screenshot from `console`:
+
+<p align="center">
+  <img width="700" align="center" src="https://i.imgur.com/NgqvqBO.png" alt="console"/>
+</p>
+
+Screenshot of command legends from `console`:
+
+<p align="center">
+  <img width="700" align="center" src="https://i.imgur.com/SU7wEvh.png" alt="console"/>
+</p>
+
+## 👋 A few words from me
+
+Again, I would like to thank [@clarkwinkelmann](https://discuss.flarum.org/u/clarkwinkelmann) for directing the concept of execution - not creating an extension to flarum, but a simple package. It gave me an effect similar to the `plug&play` I wanted to get from the beginning. Why again? Because with the previous extension he also helped me. I literally love you, [@clarkwinkelmann](https://discuss.flarum.org/u/clarkwinkelmann).
+
+The extension is aimed at developers and individuals who do not have apache or nginx installed. With one command you can display the forum locally. If necessary, you can change the port and domain to a different one than the `localhost`.
+
+## 🔗 Links
+
+- [Flarum Discuss post](https://discuss.flarum.org/d/22748-serve-by-fajuu)
+- [Source code on GitHub](https://github.com/Fajuu/flarum-serve)
+- [Flagrow Extension](https://flagrow.io/extensions/Fajuu/flarum-serve)
+- [Download via Packagist](https://packagist.org/packages/Fajuu/flarum-serve)
+
+## 📝 License
+
+Copyright © 2020 [Fajuu](https://mykill.pl/u/Fajuu).<br /> This project is [MIT](https://github.com/Fajuu/flarum-serve/blob/master/LICENSE) licensed.
+
+---
+
+_Extension created by [@Fajuu](https://github.com/Fajuu), the owner of [My Kill](https://mykill.pl)_
---
+++ src/Commands/ServeCommand.php
@@ -0,0 +1,113 @@
+<?php namespace Fajuu\Serve\Commands;
+
+use Flarum\Console\AbstractCommand;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Bundle\WebServerBundle\WebServer;
+use Symfony\Bundle\WebServerBundle\WebServerConfig;
+
+use Symfony\Component\Console\Output\ConsoleOutputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Process\Process;
+
+class ServeCommand extends AbstractCommand
+{
+  protected function configure()
+  {
+    $this->setName('serve')
+      ->setDescription('Serve the application on the PHP development server')
+      ->addOption('host', null, InputOption::VALUE_OPTIONAL, 'The host address to serve the application on.', '127.0.0.1')
+      ->addOption('port', 'p', InputOption::VALUE_OPTIONAL, 'The port to serve the application on.', 80);
+  }
+  protected function fire()
+  {
+    $input = $this->input;
+    $output = $this->output;
+
+    $io = new SymfonyStyle($input, $output);
+
+    $callback = null;
+    $disableOutput = false;
+
+    if ($output->isQuiet()) {
+      $disableOutput = true;
+    } else {
+      $callback = function ($type, $buffer) use ($output) {
+        if (Process::ERR === $type && $output instanceof ConsoleOutputInterface) {
+          $output = $output->getErrorOutput();
+        }
+        $output->write($buffer, false, OutputInterface::OUTPUT_RAW);
+      };
+    }
+
+    try {
+      $server = new WebServer();
+
+      $config = new WebServerConfig(app()->publicPath(), 'dev', $input->getOption('host') . ':' . $input->getOption('port'));
+
+      $getAddress = 'http://' . $config->getAddress();
+      $message = sprintf('Server listening on %s', $getAddress);
+
+      if ('' !== ($displayAddress = $config->getDisplayAddress())) {
+        $message = sprintf('Server listening on all interfaces, port %s -- see http://%s', $config->getPort(), $displayAddress);
+      }
+      $io->success($message);
+
+      if (!$this->checkAddress($getAddress, app('flarum.config')['url']) and $this->parseAddress(app('flarum.config')['url']) == '127.0.0.1:80') {
+        $io->note('Use address from configuration: ' . app('flarum.config')['url']);
+      }
+
+      if ($this->checkAddress($getAddress, app('flarum.config')['url'])) {
+        $io->warning('The url in `config.php` is different from the server address. This may cause flarum errors.');
+      }
+
+      if (ini_get('xdebug.profiler_enable_trigger')) {
+        $io->comment('Xdebug profiler trigger enabled.');
+      }
+      $io->comment('Quit the server with CONTROL-C.');
+
+      $exitCode = $server->run($config, $disableOutput, $callback);
+    } catch (\Exception $e) {
+      $io->error($e->getMessage());
+
+      return 1;
+    }
+
+    return $exitCode;
+  }
+
+  protected function checkAddress($serverAddr, $configAddr)
+  {
+    $configAddr = $this->parseAddress($configAddr);
+    $serverAddr = $this->parseAddress($serverAddr);
+
+    if ($serverAddr == $configAddr) {
+      return false; // if the same
+    }
+
+    return true; // else
+  }
+
+  protected function parseAddress($addr)
+  {
+    if (strpos($addr, 'http://') !== false) {
+      $addr = str_replace('http://', null, $addr);
+      $port = 80;
+    }
+    if (strpos($addr, 'https://') !== false) {
+      $addr = str_replace('https://', null, $addr);
+      $port = 443;
+    }
+
+    if (strpos($addr, ':') !== false) {
+      $port = (int) explode(':', $addr, 2)[1];
+      $addr = explode(':', $addr, 2)[0];
+    }
+
+    if ($addr == 'localhost' or $addr == '127.0.0.1') {
+      $addr = '127.0.0.1';
+    }
+
+    return $addr . ':' . $port;
+  }
+}
